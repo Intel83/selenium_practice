91644cf5c4f96bfa197b83186f73832c4a50a6ea
Marcin Gryczko <mgryczko@gmail.com>
2025-08-01 11:03:51 +0200

    refactor WebDriver setup and tests for Herokuapp and Selenium web form

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # driver-related fixtures
 @pytest.fixture(scope="function")
-def driver():
+def driver_herokuapp():
     """
     Sets up the Chrome WebDriver with the necessary configurations.
     """
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://the-internet.herokuapp.com/login")
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+@pytest.fixture(scope="function")
+def driver_selenium_webform():
+    """
+    Sets up the Chrome WebDriver for the Selenium WebForm test.
+    """
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
     driver.maximize_window()
     yield driver
     driver.quit()
